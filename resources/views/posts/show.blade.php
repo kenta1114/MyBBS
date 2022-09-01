f2ab7ed7804ef2248bc80e1ff7cdc057c7c04aab
kenta1114 <61451524+kenta1114@users.noreply.github.com>
2022-09-01 13:51:10 +0900

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,14 +10,37 @@
     <h1>
         <span>{{ $post->title }}</span>
         <a href="{{ route('posts.edit', $post) }}">[Edit]</a>
-        <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete_post">
-            @method('DELETE')
+        <form method="post" action="{{ route('posts.destroy', $post) }}" id=delete_post>
+            @method('DELETE')  
             @csrf
 
             <button class="btn">[x]</button>
         </form>
     </h1>
-    <p>{!! nl2br(e($post->body)) !!}</p>
+    <p>{!! nl2br(e($post->body)) !!}</p>  <!--改行と悪意のあるコードを防ぐ-->
+
+    <h2>Comments</h2>
+    <ul>
+        <li>
+            <form method="post" action="{{ route('comments.store',$post) }}" class="comment-form">
+                @csrf
+
+                <input type="text" name="body">
+                <button>Add</button>
+            </form>
+        </li>
+        @foreach ($post->comments()->latest()->get() as $comment)
+            <li>
+                {{ $comment->body }}
+                <form method="post" action="{{route('comments.destroy',$comment)}}" class="delete-comment">
+                    @method('DELETE')
+                    @csrf
+
+                    <button class="btn">[×]</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
 
     <script>
     	'use strict';
@@ -29,12 +52,21 @@
                 if(!confirm('Sure to delete?')){
                     return;
                 }
-                
-                 e.target.submit();
-                
+
+                e.target.submit();
+            });
+
+            document.querySelectorAll('.delete-comment').forEach(form =>{
+                form.addEventListener('submit', e =>{
+                    e.preventDefault();
+
+                    if(!confirm('Sure to delete?')) {
+                        return;
+                    }
+
+                    form.submit();
+                });
             });
         }
     </script>
-
 </x-layout>
-
